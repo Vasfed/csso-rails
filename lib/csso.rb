03417cf614b1c79674659721ee80cc24395f071a
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-06-07 01:00:52 +0700

    Add Rails 4 support

--- lib/csso.rb
+++ lib/csso.rb
@@ -4,10 +4,17 @@ require 'csso/compressor'
 
 module Csso
 
-  @csso = Csso::JsLib.new
-
   def self.js_api
-    @csso
+    @csso ||= Csso::JsLib.new
+  end
+
+  def self.install(sprockets)
+    if sprockets.respond_to? :register_compressor
+      sprockets.register_compressor('text/css', :csso, Compressor.new)
+      sprockets.css_compressor = :csso
+    else
+      Sprockets::Compressors.register_css_compressor(:csso, 'Csso::Compressor', :default => true)
+    end
   end
 
   def self.optimize(css, maniac_mode=false, structural_optimization=true)
--- lib/csso/compressor.rb
+++ lib/csso/compressor.rb
@@ -1,17 +1,9 @@
 module Csso
-  class CssCompressor
+  class Compressor
     def compress(css)
       require 'csso'
       #TODO: settings?
       Csso.optimize(css, true)
-    end
-
-    def self.register!
-      if Sprockets.respond_to? :register_compressor
-        Sprockets.register_compressor('text/css', COMPRESSOR_SYM, 'Csso::CssCompressor')
-      else
-        Sprockets::Compressors.register_css_compressor(COMPRESSOR_SYM, 'Csso::CssCompressor', :default => true)
-      end
     end
   end
 end
